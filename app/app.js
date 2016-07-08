d7b574c1294ecc1cd474075736bf466adff4da14
kpagcha <4d.garcia6@gmail.com>
2016-07-08 21:03:56 +0200

    View timetable with unavailable players & assigned timeslots

--- app/app.js
+++ app/app.js
@@ -59,22 +59,21 @@ app.controller('TournamentController', function ($scope, $rootScope, service, to
     if (index < 0 || index >= events.length)
       return;
 
-    var event = events[index];
-    $scope.event = event;
+    $scope.event = events[index];
 
     $scope.players = [];
     $scope.venues = [];
     $scope.timeslots = [];
 
-    event.players.forEach(function (i) {
+    $scope.event.players.forEach(function (i) {
       $scope.players.push(allPlayers[i]);
     });
 
-    event.localizations.forEach(function (i) {
+    $scope.event.localizations.forEach(function (i) {
       $scope.venues.push(allVenues[i]);
     });
 
-    event.timeslots.forEach(function (i) {
+    $scope.event.timeslots.forEach(function (i) {
       $scope.timeslots.push(allTimeslots[i]);
     });
   };
@@ -91,16 +90,68 @@ app.controller('TournamentController', function ($scope, $rootScope, service, to
     return allTimeslots[index];
   };
 
+  $scope.getTimeslotDisplay = function (index) {
+    var t = allTimeslots[index];
+    var start = t.start;
+
+    var str = "Timeslot " + t.chronologicalOrder;
+    if (!start)
+      return str;
+
+    switch (t.start.type) {
+      case "DayOfWeek":
+      case "Month":
+      case "Year":
+      case "LocalTime":
+        str = t.start.value;
+        break;
+      case "MonthDay":
+        str = moment().month(t.start.value.month - 1).date(t.start.value.dayOfMonth).format("MMM Do");
+        break;
+      case "LocalDate":
+        str = moment(timeslot.start.value).format("MMM Do YYYY");
+        break;
+      case "LocalDateTime":
+        str = moment(timeslot.start.value).format("MMM Do YYYY, HH:mm");
+        break;
+    }
+    return str;
+  };
+
+  $scope.getDuration = function (timeslot) {
+    if (!timeslot.duration)
+      return "";
+
+    var str = timeslot.duration.value + " " + timeslot.duration.type;
+    if (str.endsWith('s') && timeslot.duration.value < 1)
+      str = str.substr(0, str.length - 1);
+    return str;
+  };
+
   $scope.isBreak = function (index) {
-    if (!$scope.event)
+    if (!$scope.event || !$scope.event.breaks)
       return false;
     return $.inArray($scope.event.timeslots[index], $scope.event.breaks) != -1;
+  };
+
+  $scope.isPlayerUnavailable = function (playerIdx, timeslotIdx) {
+    if (!$scope.event || !$scope.event.unavailablePlayers)
+      return false;
+    return $.inArray($scope.event.timeslots[timeslotIdx],
+      $scope.event.unavailablePlayers[$scope.event.players[playerIdx]]) != -1;
+  };
+
+  $scope.isTimeslotAssigned = function (playerIdx, timeslotIdx) {
+    if (!$scope.event || !$scope.event.playersAtTimeslots)
+      return false;
+    return $.inArray($scope.event.timeslots[timeslotIdx],
+      $scope.event.playersAtTimeslots[$scope.event.players[playerIdx]]) != -1;
   };
 
   /* ATECIÓN ESTÁ ESTO DE FORMA TEMPORAL PARA VER COMO QUEDA LA PAGINA DE EVENTO.
    QUITAR CUANDO ESTÉ TERMINADA
    */
-  $scope.showEvent(2);
+  $scope.showEvent(0);
   /**
    * FIN. CUANDO ESTE TERMINADO BORRAR LO DE ARRIBA
    */
